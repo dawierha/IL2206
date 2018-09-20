b9eeccd02a4a93f0918d128283072159094a26ac
KTH Student <student@il2206.vm>
2018-09-20 14:50:25 +0200

    completed part 3

--- hardware/software/nios2_ht18_Eriksson_keyserlingk/obj/default/hello_world_small.d
+++
@@ -1,4 +0,0 @@
-obj/default/hello_world_small.o: hello_world_small.c \
-  ../nios2_ht18_Eriksson_keyserlingk_bsp//HAL/inc/sys/alt_stdio.h
-
-../nios2_ht18_Eriksson_keyserlingk_bsp//HAL/inc/sys/alt_stdio.h:
